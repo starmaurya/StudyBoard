cfb03dbe52b4dcbb8e36e8af628c0b3d46e7cb48
Tarachand Maurya <starmaurya01@gmail.com>
2025-09-07 18:23:31 +0530

    init: initial StudyBoard project setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# 📖 StudyBoard
+An offline whiteboard app built in Kotlin using MVVM architecture.
---
+++ app/src/main/java/com/starmaurya/whiteboard/views/WhiteboardView.kt
@@ -0,0 +1,11 @@
+package com.starmaurya.whiteboard.views
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class WhiteboardView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : View(context, attrs) {
+
+}
